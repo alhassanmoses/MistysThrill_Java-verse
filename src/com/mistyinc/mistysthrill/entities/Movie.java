71d7494acaa0fb447ffc1d90df09bc91f2a8fc24
alhassanmoses <alhassanmoses.amw@gmail.com>
2021-08-30 13:49:50 +0000

    initail commit...

---
+++ src/com/mistyinc/mistysthrill/entities/Movie.java
@@ -0,0 +1,49 @@
+package com.mistyinc.mistysthrill.entities;
+
+public class Movie extends Bookmark {
+    private int releasedYear;
+    private String[] cast;
+    private String[] directors;
+    private String genre;
+    private double imdbRating;
+
+    public int getReleasedYear() {
+        return releasedYear;
+    }
+
+    public void setReleasedYear(int releasedYear) {
+        this.releasedYear = releasedYear;
+    }
+
+    public String[] getCast() {
+        return cast;
+    }
+
+    public void setCast(String[] cast) {
+        this.cast = cast;
+    }
+
+    public String[] getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(String[] directors) {
+        this.directors = directors;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+}
---
+++ src/com/mistyinc/mistysthrill/managers/UserManager.java
@@ -0,0 +1,5 @@
+package com.mistyinc.mistysthrill.managers;
+
+public class UserManager {
+
+}
